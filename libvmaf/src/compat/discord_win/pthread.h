ab7c859539fef25cafbeec40f099fe2120c378ab
Shaun Hedrick <shaun.hedrick@discordapp.com>
2023-09-05 15:20:49 -0700

    [VMAF] Improved the pthread shim used on Windows (#6)
    This replaces the very problematic `#define`-based pthread replacement with a much-less-but-still-problematic C implementation.
    
    - Since they are functions, this fixes bugs in the `#define`s that didn't "return" values for a few functions
    - Allows state to exist, so that we don't have to quirk the callback signature for `pthread_create`
    - Notably does **not** implement the entire surface area of functions. They will emit failures if certain arguments (unused by VMAF) are used.
    - Biggest remaining issue with this implementation is if a thread is started, detached, and the process exits soon after, you can hit some UB (read: crash) related to not joining the thread and it executing after important things are destroyed via `main` exit

--- libvmaf/src/compat/discord_win/pthread.h
+++ libvmaf/src/compat/discord_win/pthread.h
@@ -1,37 +1,23 @@
 #ifndef __PTHREAD_H
 #define __PTHREAD_H
 
-// TODO: these are very quick and dirty. Notably, _beginthreadex has troubling semantics for its return value
-
 #include <windows.h>
 #include <process.h>
 
-// Mutexes
 typedef CRITICAL_SECTION pthread_mutex_t;
-#define pthread_mutex_init(a, b) 0; InitializeCriticalSection(a)
-#define pthread_mutex_lock(a) EnterCriticalSection(a)
-#define pthread_mutex_unlock(a) LeaveCriticalSection(a)
-#define pthread_mutex_destroy(a) DeleteCriticalSection(a)
-
-// Condition Variables
 typedef CONDITION_VARIABLE pthread_cond_t;
-#define pthread_cond_init(a, b) 0; InitializeConditionVariable(a)
-#define pthread_cond_wait(a, b) SleepConditionVariableCS(a, b, INFINITE)
-#define pthread_cond_signal(a) WakeConditionVariable(a)
-#define pthread_cond_broadcast(a) WakeAllConditionVariable(a)
-#define pthread_cond_destroy(a) /*no-op in windows*/
-
-// Thread Creation
 typedef HANDLE pthread_t;
-#define pthread_create(a, b, c, d) *a = (HANDLE) _beginthreadex(NULL, 0, c, d, 0, NULL)
-#define pthread_join(a, b) WaitForSingleObject(a, INFINITE)
-#define pthread_detach(a) CloseHandle(a)
 
-/*
-typedef HANDLE pthread_t;
-#define pthread_create(a, b, c, d) *a = 0
-#define pthread_join(a, b) 
-#define pthread_detach(a) 
-*/
+int pthread_mutex_init(pthread_mutex_t* mutex, const void* mustBeUnused);
+int pthread_mutex_lock(pthread_mutex_t* mutex);
+int pthread_mutex_unlock(pthread_mutex_t* mutex);
+int pthread_mutex_destroy(pthread_mutex_t* mutex);
+int pthread_cond_init(pthread_cond_t* cond, const void* mustBeUnused);
+int pthread_cond_wait(pthread_cond_t* cond, pthread_mutex_t* mutex);
+int pthread_cond_signal(pthread_cond_t* cond);
+int pthread_cond_broadcast(pthread_cond_t* cond);
+int pthread_cond_destroy(pthread_cond_t* cond);
+int pthread_create(pthread_t* thread, void* mustBeUnused, void* (*startFn)(void*), void* arg);
+int pthread_detach(pthread_t thread);
 
 #endif
